9177aca4c06814bde092705c4d98825784726a36
wang <wangguogangll@126.com>
2023-03-30 11:37:32 +0800

    Update detect-route.sh

--- docker-root/usr/local/bin/detect-route.sh
+++ docker-root/usr/local/bin/detect-route.sh
@@ -10,12 +10,12 @@ for i in $(ip route show); do IFS=' '; ip route add $i table 2 ; done
 ## 回包路由
 ip rule add iif tun0 table 2
 ## 确定策略路由方式
-ip rule add iif lo table 2 sport 1080
+ip rule add iif lo table 2 sport 1081
 if ip rule show iif lo table 2 | grep sport >/dev/null ; then
 	echo 'open_port() { ip rule add iif lo table 2 sport $1; }'
 	echo 'close_port() { ip rule del iif lo table 2 sport $1; }'
-elif iptables -t mangle -A OUTPUT -j MARK --set-mark 1 -p tcp --sport 1080 2>/dev/null ; then
-	iptables -t mangle -D OUTPUT -j MARK --set-mark 1 -p tcp --sport 1080
+elif iptables -t mangle -A OUTPUT -j MARK --set-mark 1 -p tcp --sport 1081 2>/dev/null ; then
+	iptables -t mangle -D OUTPUT -j MARK --set-mark 1 -p tcp --sport 1081
 	ip rule add fwmark 1 table 2
 	echo 'open_port() { iptables -t mangle -I OUTPUT -j MARK --set-mark 1 -p tcp --sport $1; }'
 	echo 'close_port() { iptables -t mangle -D OUTPUT -j MARK --set-mark 1 -p tcp --sport $1; }'
@@ -26,5 +26,5 @@ else
 	     "(refer to https://github.com/Hagb/docker-easyconnect/tree/master/doc/route.md)" >&2
 
 fi
-ip rule del iif lo sport 1080 table 2
+ip rule del iif lo sport 1081 table 2
 
